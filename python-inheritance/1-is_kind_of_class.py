1208a0443624ad2472c4c19fd7ef4cbabee945fc
aabbas99 <aabbas99@terpmail.umd.edu>
2023-09-01 19:53:33 -0700

    Fixed file issue

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Check if an object is an instance of, or if the obj\
+is an instance of a class inheriting from a_class.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of or if the obj\
+    is an instance of a class inheriting from a_class.
+    """
+    return isinstance(obj, a_class)
